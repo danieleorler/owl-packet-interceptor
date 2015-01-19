51631d75262efa0e03523cf3a8baec19daf2551c
Daniele Orler <danorler@fbk.eu>
2015-01-19 09:11:12 +0100

    polishing

---
+++ src/Storage.php
@@ -0,0 +1,19 @@
+<?php
+
+class Storage
+{
+    private $dbConnection;
+
+    public function __construct()
+    {
+        $this->dbConnection = new PDO('mysql:dbname=db_name;host=localhost', 'db_user', 'db_password', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
+    }
+
+    public function log($value)
+    {
+        $sql = 'INSERT INTO electricity(system_id,watts) VALUES(1,:watts)';
+        $sth = $this->dbConnection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':watts' => intval($value)));
+        $sth->closeCursor();
+    }
+}
